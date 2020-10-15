7dc0c5e5846b62bd91452edd2ef14407508d787f
Lucas Vogelsang <l@lucasvo.com>
2020-10-15 09:20:52 +0000

    dapp init TinlakeSpells

---
+++ src/TinlakeSpells.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.6.7;
+
+import "ds-test/test.sol";
+
+import "./TinlakeSpells.sol";
+
+contract TinlakeSpellsTest is DSTest {
+    TinlakeSpells spells;
+
+    function setUp() public {
+        spells = new TinlakeSpells();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
