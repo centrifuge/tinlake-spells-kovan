eec7f5126c488bdd4835a3f1b1c337e14881955b
Adam Stockermans <adamstox@protonmail.com>
2022-03-03 14:34:52 -0500

    Update IPFS with pool-admin version

--- src/spell.sol
+++ src/spell.sol
@@ -76,7 +76,7 @@ contract TinlakeSpell is Addresses {
 
     address public POOL_REGISTRY = 0xddf1C516Cf87126c6c610B52FD8d609E67Fb6033;
 
-    string constant public IPFS_HASH = "QmcVoEqdRjNxwaQmooWjDguBr4FjQ214ZUZwhwTKjJrtoK";
+    string constant public IPFS_HASH = "QmYfAJzwQyp2FtFRiL56RMNrmF6cm38BPTFJADG9MxJtgj";
 
     uint256 constant ONE = 10**27;
     address self;
